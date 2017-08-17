5d32e4ced9709ae32ea03beb71f596100b94f03c
mohrai <mohrai@DIN56001190.corp.capgemini.com>
2017-08-17 14:28:48 +0530

    remove unused imports

--- CGBillingSystems/src/main/java/com/cg/billing/beans/Plan.java
+++ CGBillingSystems/src/main/java/com/cg/billing/beans/Plan.java
@@ -1,6 +1,5 @@
 package com.cg.billing.beans;
 
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
